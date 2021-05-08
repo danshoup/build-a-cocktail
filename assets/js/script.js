6c7f8dfb40436f5c69ace7ac672fb72fa7e78382
Curtisaurus <ccdrullinger@gmail.com>
2021-05-08 13:49:26 -0400

    added modal functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,12 @@ var spiritDropdown = $('#spirit-select');
 // gets bulma grid container for cocktail list items
 var cocktailList = $('#card-container');
 
+// gets ingredient list ul container
+var ingredientList = $('#ingList')
+
+// gets modal element
+var recipeModal = $('#recipeModal')
+
 //runs on dropdown value change
 spiritDropdown.on('change', function(event) {
     // gets spirit upon selection
@@ -43,6 +49,8 @@ function displayCocktails(cocktailsData) {
 
         createCard(drinksArray[i]);
     }
+
+    console.log($('.card'))
 }
 
 function createCard(drink) {
@@ -70,20 +78,17 @@ function createCard(drink) {
             )
         )
     )
-
-    cocktailList.append(newCard)
+    
+    // adds card to main part of page below dropdown
+    cocktailList.append(newCard)    
 
 }
 
 // executes upon clicking a cocktail in the list
-$('ul').on("click", function(event) {
-    event.preventDefault();
+$('#card-container').on("click", "button", function(event) {
 
-    // pulls text from clicked list item
-    strCocktail = event.target.textContent;
-
-    //gets drink id stored in li element id
-    drinkId = event.target.getAttribute('id')
+    // gets drink id from card around button
+    drinkId = $(event.target).parents('.card').attr('id')
 
     // runs the recipe call from api search for cocktail name
     getRecipe(drinkId);
@@ -107,15 +112,14 @@ function getRecipe(drinkId) {
 
 function displayRecipe(data) {
 
-    // clears old text from ul container
-    cocktailList.html("");
+    // clears ingredient list items
+    ingredientList.html('')
 
     // pulls recipe details object from single object array, represented as a value of "drinks" key
     recipeDetails = data.drinks[0]
 
-    //logs cocktail name and add it as a bolded list item element inside ul
-    cocktailName = $('<li></li>').text(recipeDetails.strDrink).css("font-weight", "bold").css("list-style", "none");
-    cocktailList.append(cocktailName);
+    //updates modal with cocktail name
+    $('p.modal-card-title').text(recipeDetails.strDrink);
     
     for (i = 1; i < 15; i++) {
         // assigns key for ingredient number to string (i.e. strIngredient1 is the key for the value of "Lime")
@@ -124,14 +128,19 @@ function displayRecipe(data) {
         if (recipeDetails[ingredientNum]) {
             // creates list item with indredient text and appends to ul container under cocktail name
             ingredientItem = $('<li></li>').text(recipeDetails[ingredientNum])
-            cocktailList.append(ingredientItem)
+            ingredientList.append(ingredientItem)
         } else {
             // breaks out of loop upon first "null" ingredient
             break
         }
     }
 
-    //displays recipe recipe instructions below ingredients in italics
-    instructions = $('<li></li>').text(recipeDetails.strInstructions).css("font-style", "italic").css("list-style", "none")
-    cocktailList.append(instructions)
+    //displays recipe recipe instructions in p element below ingredient ul
+    $('#instructions').text(recipeDetails.strInstructions)
+
+    recipeModal.toggleClass("is-active");
 }
+
+recipeModal.on("click", "#close", function() {
+    recipeModal.toggleClass("is-active")
+})
